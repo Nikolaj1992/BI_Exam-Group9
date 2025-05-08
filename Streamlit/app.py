c25404ea18eaa3244b89a0817765438efd0092ec
Obigonemad <vuf.foretrukne@gmail.com>
2025-05-08 12:16:48 +0200

    minor

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -2,13 +2,16 @@ import streamlit as st
 import read_image
 import read_api
 
+from PIL import Image
+logo = Image.open('./media/esc.jpg')
 
 # Function to display the homepage content
 def show_homepage():
-    st.title('Homepage')
+    st.image(logo)
+    st.title('Exam project in BI')
     st.write('This Project is an analysis of European Song contest.')
-    
     st.write("Made by: Jenny, David, Nikolaj and Patrick")
+    st.write("Sem 4, BI, 2025")
 
 # Main function that runs the app
 def main():
