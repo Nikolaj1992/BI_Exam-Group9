fccd845989cb36bca3eda57c7868d276270d92a3
Obigonemad <vuf.foretrukne@gmail.com>
2025-05-20 10:59:21 +0200

    cleaned  up and comment on naives bayes

--- Streamlit/pages/classification.py
+++ Streamlit/pages/classification.py
@@ -16,12 +16,11 @@ def show_classification ():
     draw = st.number_input("Final Draw Position", min_value=1, max_value=27, value=25)
     televote_points = st.number_input("Televote Points", min_value=0, max_value=500, value=120)
     jury_points = st.number_input("Jury Points", min_value=0, max_value=500, value=130)
-    televote_votes = st.number_input("Televote Votes", min_value=0, max_value=50, value=25)
-    jury_votes = st.number_input("Jury Votes", min_value=0, max_value=50, value=25)
+
 
     # Prediction button
     if st.button("Predict Top 10 Placement"):
-        sample = np.array([[draw, televote_points, jury_points, televote_votes, jury_votes]])
+        sample = np.array([[draw, televote_points, jury_points]])
         prediction = model.predict(sample)
         result = "✅ YES – Likely Top 10!" if prediction[0] == 1 else "❌ NO – Not in Top 10"
         st.subheader("Prediction Result:")
